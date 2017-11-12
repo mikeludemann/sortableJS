47186d895ebe4cdc975041eea2038d1b50dd4aa2
Mike Ludemann <mike-ludemann@hotmail.de>
2017-11-12 03:15:16 +0100

    First Commit with all data informations

---
+++ README.md
@@ -0,0 +1,23 @@
+# sortableJS
+
+Sortable elements in ASC and DESC format. 
+Toggle function with ASC and DESC.
+
+```code
+
+sortable.sortASC(mainElement, subElement, sortingElement, clickElement);
+
+With testing parameter
+sortable.sortASC("#container", ".container__sub", "div", "#click");
+
+sortable.sortDESC(mainElement, subElement, sortingElement, clickElement);
+
+With testing parameter
+sortable.sortDESC("#container", ".container__sub", "div", "#click");
+
+sortable.sortToggle(mainElement, subElement, sortingElement, clickElement);
+
+With testing parameter
+sortable.sortToggle("#container", ".container__sub", "div", "#click");
+
+```
---
+++ sortable.js
@@ -0,0 +1,89 @@
+var sortable = (function(){
+
+	var sortASC = function(mainElement, subElement, sortingElement, clickElement){
+  	
+        $(clickElement).click(function () {
+            var ascElements = $(subElement).sort(function (a, b) {
+                return $(a).find(sortingElement).text() > $(b).find(sortingElement).text();
+            });
+            $(mainElement).html(ascElements);
+        });
+  
+    }
+  
+    var sortDESC = function(mainElement, subElement, sortingElement, clickElement){
+        
+        $(clickElement).click(function (e) {
+                e = e || event;
+            e.preventDefault();
+            var descElements = $(subElement).sort(function (a, b) {
+                return $(a).find(sortingElement).text() < $(b).find(sortingElement).text();
+            });
+            $(mainElement).html(descElements);
+        });
+    
+    }
+
+    var convertStringToNumber = function(value){
+        
+        // Value after commata with commata
+        var commaWithValue = value.substr(-3,3);
+        
+        // Only the commata char
+        var commaOnly = value.substr(-3,1);
+        
+        // Replace commata to point
+        var commaToPoint = commaWithValue.replace(",",".");
+        
+        // The value before commata char
+        var valueForComma = value.replace(commaWithValue,"");
+        
+        // Convert any thousand format from currency to a valid number
+        var valueToNumber = valueForComma.replace(".","");
+        
+        // Get the complete valid number
+        var completeNumber = valueToNumber + "" + commaToPoint;
+        
+        return parseFloat(completeNumber);
+          
+    }
+      
+    var sortToggle = function(mainElement, subElement, sortingElement, clickElement){
+    
+        var ascending = false;
+        
+        $(clickElement).click(function () {
+
+            var elements = $(subElement);
+
+            var sortingElements = elements.sort(function (a, b) {
+
+            if(/\d/.test($(a).find(sortingElement).html())){
+
+                return (ascending == (convertStringToNumber($(a).find(sortingElement).html()) < convertStringToNumber($(b).find(sortingElement).html()))) ? 1 : -1;
+
+            } else {
+
+                return (ascending == ($(a).find(sortingElement).html() < $(b).find(sortingElement).html())) ? 1 : -1;
+
+            }
+
+            });
+
+            ascending = ascending ? false : true;
+
+            $(mainElement).html(sortingElements);	
+
+        });
+    
+    }
+  
+  return {
+  	
+    sortASC: sortASC,
+    sortDESC: sortDESC,
+    sortToggle: sortToggle
+  
+  }
+
+})();
